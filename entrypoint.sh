ea817f61f2819e0f6d9695e4bceb132f7b5bb101
metaVariable <aoyama@bi.cs.titech.ac.jp>
2019-10-17 16:57:09 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -eux
 
-user=metaVariable
-repo=LNCS-template
+GITHUB_OWNER=${GITHUB_OWNER:-"USER"}
+GITHUB_REPO=${GITHUB_REPO:-"REPO"}
 
 # build pdf (change if necessary)
 pdflatex samplepaper.tex
 
 # create release
-res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/$user/$repo/releases \
+res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/releases \
 -d "
 {
   \"tag_name\": \"v$GITHUB_SHA\",
@@ -22,6 +22,6 @@ res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com
 rel_id=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
 
 # upload built pdf
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$user/$repo/releases/${rel_id}/assets?name=samplepaper.pdf\
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/releases/${rel_id}/assets?name=samplepaper.pdf\
   --header 'Content-Type: application/pdf'\
   --upload-file samplepaper.pdf
